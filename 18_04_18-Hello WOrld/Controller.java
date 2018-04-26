0d85c92408ec65a772b6e2b5f23645af74c55d85
HumbertoGV <35930096+HumbertoGV@users.noreply.github.com>
2018-04-25 17:05:49 -0700

    Add files via upload

---
+++ 18_04_18-Hello WOrld/Controller.java
@@ -0,0 +1,10 @@
+	public class Controller{
+	
+	public static String HelloWorld() { 
+    	return "Hello World!";
+	}
+
+	public static int suma(int x, int y){
+		return x+y;
+	}
+}
---
+++ 18_04_18-Hello WOrld/Test.java
@@ -0,0 +1,7 @@
+	public class Test{
+	public static void main(String[] args) {
+
+		System.out.println(Controller.HelloWorld());
+		System.out.println(Controller.suma(20,20));
+	}
+}
